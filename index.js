1f570448e4f2572c29b10c82c0697ba47a51967c
darcioSoares <darciosoares28@gmail.com>
2020-04-12 19:00:26 -0300

    cadastro de usuario

---
+++ index.js
@@ -0,0 +1,42 @@
+
+let form = document.querySelectorAll(".form [js]");
+let user = {}
+let tbody = document.querySelector("tbody")
+
+function addLine(dataUser){
+
+    let tr = document.createElement('tr')
+
+    tr.innerHTML = `<tr>
+    <td>${dataUser.nome}</td>
+    <td>${dataUser.genero}</td>
+    <td>${dataUser.email}</td>
+    <td>${dataUser.setor}</td>
+    <td>${1}</td>
+    </tr>
+    `
+    tbody.appendChild(tr)
+
+    console.log(dataUser)
+}     
+
+    // esse form esta completo
+    let formSubmit = document.querySelector(".form")
+
+    formSubmit.addEventListener("submit", function(e){
+        event.preventDefault()
+
+        form.forEach(function(field){
+
+            if(field.name == "genero"){
+                if(field.checked){
+                    user[field.name] = field.value
+                }
+            }else{
+                user[field.name] = field.value
+            }
+        })
+
+        addLine(user)
+    })
+
